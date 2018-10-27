2506ef6368129926ad03ba76c88ffea4bfeaa47d
dyl321 <44518188+dyl321@users.noreply.github.com>
2018-10-27 16:59:02 +1100

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,20 @@
+//document.write("Hello World");
+console.log("It worked");
+
+// alternative to DOMContentLoaded event
+document.onreadystatechange = function () {
+    if (document.readyState === "interactive") {
+      initApplication();
+    }
+  }
+
+  function initApplication () {
+      let el = document.getElementById("RightOrWrong");
+      console.log(el);
+
+      el.addEventListener("change", function() {
+          console.log(el.checked);
+          let el2 = document.getElementById("result");
+          el2.innerHTML = "checked: " + el.checked;
+      });
+  }
